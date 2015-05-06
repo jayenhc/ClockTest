032a7ff34a46054b1894a50fd7542b3c75890686
jayen chondigara <jayenhc@yahoo.com>
2015-05-06 19:38:30 +0100

    create ClockPuzzle class with test case

--- TestClock.java
+++ TestClock.java
@@ -1,14 +1,24 @@
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 
 public class TestClock extends TestCase {
 
+	ClockPuzzle clock ;
 	protected void setUp() throws Exception {
 		super.setUp();
+		clock = new ClockPuzzle();
+		
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 
+	@Test
+	public void testClockObjNotNull(){
+		assertNotNull(clock);
+		
+	}
 }
